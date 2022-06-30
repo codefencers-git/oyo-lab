31ace17e8e4ebb2e244304e79a81d88ffef810a9
Sneha Vansjalia <sneha.vansjalia@codefencers.com>
2022-06-30 18:56:40 +0530

    add button in sort by modal

--- lib/screens/laboratory/laboratory_tile.dart
+++ lib/screens/laboratory/laboratory_tile.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
-import 'package:get/get.dart';
-import 'package:oyo_labs/routes.dart';
 import 'package:oyo_labs/themedata.dart';
 
 import '../../widgets/buttons/round_book_slot.dart';
+
+import 'laboratory_detail.dart';
 
 class LaboratoryTile extends StatelessWidget {
   const LaboratoryTile({Key? key}) : super(key: key);
@@ -35,91 +35,85 @@ class LaboratoryTile extends StatelessWidget {
             // height: height / 3,
             fit: BoxFit.cover,
           ),
-          Padding(
-            padding: const EdgeInsets.only(left: 8.0, top: 5),
-            child: Text(
-              "A002",
-              style: TextStyle(fontSize: 8, color: ThemeClass.orangeColor),
+          const Padding(
+            padding: EdgeInsets.only(
+              left: 8.0,
+              top: 10,
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 8.0, top: 5),
             child: Text(
-              "ABO Group & RH Type",
+              "Green Cross Laboratory",
               style: TextStyle(fontSize: 10),
             ),
           ),
+          const Padding(
+            padding: EdgeInsets.only(
+              left: 8.0,
+              top: 5,
+            ),
+            child: Text(
+              "1 Km Away",
+              style: TextStyle(fontSize: 8),
+            ),
+          ),
+          const SizedBox(
+            height: 5,
+          ),
           Padding(
             padding: const EdgeInsets.only(
-              left: 8.0,
+              left: 5.0,
+              top: 0.2,
             ),
-            child: Text(
-              "(Blood Group)",
-              style: TextStyle(fontSize: 8, color: ThemeClass.greyColor),
+            child: Row(
+              children: [
+                RatingBar(
+                  initialRating: 3,
+                  direction: Axis.horizontal,
+                  allowHalfRating: true,
+                  itemSize: 16,
+                  itemCount: 5,
+                  ignoreGestures: true,
+                  ratingWidget: RatingWidget(
+                    full: _image('assets/images/full_star.png'),
+                    half: _image('assets/images/half_star.png'),
+                    empty: _image('assets/images/blank_star.png'),
+                  ),
+                  itemPadding: const EdgeInsets.symmetric(horizontal: 0.7),
+                  onRatingUpdate: (rating) {
+                    print(rating);
+                  },
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8),
+                  child: Text(
+                    '4.0',
+                    style: TextStyle(
+                        fontSize: 8,
+                        color: ThemeClass.redColor,
+                        fontWeight: FontWeight.w900),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 8),
+                  child: Text(
+                    '(120)',
+                    style: TextStyle(
+                        fontSize: 8,
+                        color: ThemeClass.blackColor,
+                        fontWeight: FontWeight.w900),
+                  ),
+                ),
+              ],
             ),
           ),
           Padding(
-            padding: const EdgeInsets.only(left: 8.0, top: 5),
-            child: Text(
-              "(W B-ED TA (3ml))",
-              style: TextStyle(fontSize: 8, color: ThemeClass.orangeColor),
-            ),
-          ),
-
-          // Padding(
-          //   padding: const EdgeInsets.only(
-          //     left: 5.0,
-          //     top: 0.2,
-          //   ),
-          //   child: Row(
-          //     children: [
-          //       RatingBar(
-          //         initialRating: 3,
-          //         direction: Axis.horizontal,
-          //         allowHalfRating: true,
-          //         itemSize: 16,
-          //         itemCount: 5,
-          //         ignoreGestures: true,
-          //         ratingWidget: RatingWidget(
-          //           full: _image('assets/images/full_star.png'),
-          //           half: _image('assets/images/half_star.png'),
-          //           empty: _image('assets/images/blank_star.png'),
-          //         ),
-          //         itemPadding: const EdgeInsets.symmetric(horizontal: 0.7),
-          //         onRatingUpdate: (rating) {
-          //           print(rating);
-          //         },
-          //       ),
-          //       Padding(
-          //         padding: const EdgeInsets.only(left: 8),
-          //         child: Text(
-          //           '4.0',
-          //           style: TextStyle(
-          //               fontSize: 8,
-          //               color: ThemeClass.redColor,
-          //               fontWeight: FontWeight.w900),
-          //         ),
-          //       ),
-          //       Padding(
-          //         padding: const EdgeInsets.only(left: 8),
-          //         child: Text(
-          //           '(120)',
-          //           style: TextStyle(
-          //               fontSize: 8,
-          //               color: ThemeClass.blackColor,
-          //               fontWeight: FontWeight.w900),
-          //         ),
-          //       ),
-          //     ],
-          //   ),
-          // ),
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 8,
-            ),
+            padding: const EdgeInsets.only(top: 5),
             child: RoundButtonBookSlot(
               onTap: () {
-                Get.toNamed(Routes.labTileScreen);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const LaboratoryDetail()),
+                );
                 print("object");
               },
               label: 'Book a Slot',
